7ed27ec8c21645f5f3a32e50c143b0d136a482e1
Yakoderaa <yakodera@gmail.com>
2026-09-16 19:55:31 -0300

    Compilar nuevo mapeo exclusivo por bordes

--- build.ps1
+++ build.ps1
@@ -15,7 +15,7 @@ pyinstaller --noconfirm --clean --windowed --onedir `
     --collect-all miio `
     --collect-all micloud `
     --collect-all PIL `
-    src\app_v10.py
+    src\app_v11.py
 
 $pf86 = ${env:ProgramFiles(x86)}
 $inno = @(
