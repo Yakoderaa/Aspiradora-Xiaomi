e938eb2f3be4353643326f69ed27d59bb0624014
Yakoderaa <yakodera@gmail.com>
2026-09-16 19:00:33 -0300

    Compilar versión corregida sin crash al abrir

--- build.ps1
+++ build.ps1
@@ -15,7 +15,7 @@ pyinstaller --noconfirm --clean --windowed --onedir `
     --collect-all miio `
     --collect-all micloud `
     --collect-all PIL `
-    src\app_v8.py
+    src\app_v9.py
 
 $pf86 = ${env:ProgramFiles(x86)}
 $inno = @(
