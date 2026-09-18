0c33cacde998237615a0a799eeae8d4e6fa03e67
Yakoderaa <yakodera@gmail.com>
2026-09-17 23:03:20 -0300

    V61: nuevo entrypoint

---
+++ src/main_v61.py
@@ -0,0 +1,23 @@
+import sys
+import traceback
+
+import app_v61
+import device_metadata_patch
+
+
+def main():
+    if "--packaging-smoke-test" in sys.argv:
+        return 0
+
+    device_metadata_patch.install()
+    app_v61.app_v60.app_v58.app_v57.app_v56.app_v55.app_v54.app_v53.app_v52.app_v51.app_v50.app_v49.app_v48.app_v47.app_v46.app_v45.app_v44.app_v43.app_v42.app_v41.app_v40.app_v39.app_v38.app_v37.app_v36.app_v35.app_v34.app_v33.app_v32.app_v31.app_v30.app_v29.app_v28.app_v27.app_v26.app_v25.app_v24.app_v23.app_v22.app_v21.app_v20.app_v19.app_v18.app_v17.app_v16.app_v15.app_v14.app_v13.app_v12.app_v11.app_v10.app_v9._save_crash_log("")
+    try:
+        app_v61.App().mainloop()
+    except Exception:
+        app_v61.app_v60.app_v58.app_v57.app_v56.app_v55.app_v54.app_v53.app_v52.app_v51.app_v50.app_v49.app_v48.app_v47.app_v46.app_v45.app_v44.app_v43.app_v42.app_v41.app_v40.app_v39.app_v38.app_v37.app_v36.app_v35.app_v34.app_v33.app_v32.app_v31.app_v30.app_v29.app_v28.app_v27.app_v26.app_v25.app_v24.app_v23.app_v22.app_v21.app_v20.app_v19.app_v18.app_v17.app_v16.app_v15.app_v14.app_v13.app_v12.app_v11.app_v10.app_v9._save_crash_log(traceback.format_exc())
+        raise
+    return 0
+
+
+if __name__ == "__main__":
+    raise SystemExit(main())
