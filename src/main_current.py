161f7621eac448b1e80321bd7f6c86f2049ca372
Yakoderaa <yakodera@gmail.com>
2026-09-24 02:17:32 -0300

    V155: activar mapeo firme

--- src/main_current.py
+++ src/main_current.py
@@ -1,7 +1,7 @@
 import sys
 import traceback
 
-import app_native_start
+import app_v155
 import app_v9
 import device_metadata_patch
 import windows_audio_identity
@@ -15,7 +15,7 @@ def main():
     windows_audio_identity.install()
     app_v9._save_crash_log("")
     try:
-        app_native_start.App().mainloop()
+        app_v155.App().mainloop()
     except Exception:
         app_v9._save_crash_log(traceback.format_exc())
         raise
